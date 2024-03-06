6341df29f0b6b88f5baa2cda8a57c134c7c8d12f
Arne <arnev.c.23@gmail.com>
2024-03-06 13:44:49 +0100

    add script to rotate game objects

---
+++ assignments/2-solarsystem/Assets/Scripts/RotateAround.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class RotateAround : MonoBehaviour
+{
+    public Transform target;
+    public int speed;
+
+    void Start()
+    {
+        if (target == null)
+        {
+            target = this.gameObject.transform;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(target.transform.position, target.transform.up, speed * Time.deltaTime);
+    }
+}
