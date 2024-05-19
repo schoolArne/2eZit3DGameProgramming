025a543faa77973d1a61f081359598b47312fd31
Arne <arnev.c.23@gmail.com>
2024-05-19 16:59:17 +0200

    add script to test interacting with signs

---
+++ final project/FinalProject/Assets/Scripts/Interactive Objects/InfoInteractive.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoInteractable : MonoBehaviour
+{
+    public void OnInteract()
+    {
+        Debug.Log("The sign has been interacted with!");
+    }
+}
