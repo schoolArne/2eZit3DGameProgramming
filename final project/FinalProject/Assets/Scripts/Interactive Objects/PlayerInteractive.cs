babf572fcae4ab96e1df632fb08cc532d4edd8a3
Arne <arnev.c.23@gmail.com>
2024-05-21 22:26:28 +0200

    display interaction message when player is able to interract with sign

--- final project/FinalProject/Assets/Scripts/Interactive Objects/PlayerInteractive.cs
+++ final project/FinalProject/Assets/Scripts/Interactive Objects/PlayerInteractive.cs
@@ -1,20 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerInteractive : MonoBehaviour
 {
     public float interactionRange = 5f;
+    public TMP_Text interactionMessage;
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(1))
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, interactionRange))
         {
-            Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit, interactionRange))
+            if (hit.collider.CompareTag("Interactable"))
             {
-                if (hit.collider.CompareTag("Interactable"))
+                interactionMessage.enabled = true;
+                if (Input.GetMouseButtonDown(1))
                 {
                     InfoInteractable infoInteractable = hit.collider.GetComponent<InfoInteractable>();
                     if (infoInteractable != null)
@@ -23,6 +26,14 @@ public class PlayerInteractive : MonoBehaviour
                     }
                 }
             }
+            else
+            {
+                interactionMessage.enabled = false;
+            }
+        }
+        else
+        {
+            interactionMessage.enabled = false;
         }
     }
 }
