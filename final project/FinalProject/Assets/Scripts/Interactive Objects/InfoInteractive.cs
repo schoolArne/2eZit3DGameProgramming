7cd8fc1fa7fd6760227f1144632ba678f97c863a
Arne <arnev.c.23@gmail.com>
2024-05-21 22:47:01 +0200

    modify interactable script to display message

--- final project/FinalProject/Assets/Scripts/Interactive Objects/InfoInteractive.cs
+++ final project/FinalProject/Assets/Scripts/Interactive Objects/InfoInteractive.cs
@@ -1,11 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class InfoInteractable : MonoBehaviour
 {
+    private bool CurrentlyShowingMessage = false;
+    public TMP_Text textElement;
+    public string message = "";
     public void OnInteract()
     {
-        Debug.Log("The sign has been interacted with!");
+        textElement.text = message;
+        CurrentlyShowingMessage = true;
+    }
+    public void Update()
+    {
+        if(CurrentlyShowingMessage)
+        {
+            if(Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1) || Input.GetKeyDown(KeyCode.Escape)
+            {
+                textElement.text = "";
+                CurrentlyShowingMessage = false;
+            }
+        }
     }
 }
