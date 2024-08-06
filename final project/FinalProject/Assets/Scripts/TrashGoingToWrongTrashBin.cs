32baed77fa5c6efce791edd98735f9019a69fb47
Arne <arnev.c.23@gmail.com>
2024-08-06 23:59:07 +0200

    fix trash movement

--- final project/FinalProject/Assets/Scripts/TrashGoingToWrongTrashBin.cs
+++ final project/FinalProject/Assets/Scripts/TrashGoingToWrongTrashBin.cs
@@ -8,12 +8,20 @@ public class TrashGoingToWrongTrashBin : MonoBehaviour
     private GameObject Paper;
     private GameObject PMD;
     public TrashType trashType;
+
     private GameObject target;
+    private Rigidbody rb;
     void Start()
     {
         REST = GameObject.Find("Rest");
         Paper = GameObject.Find("Papier");
         PMD = GameObject.Find("PMD");
+        rb = GetComponent<Rigidbody>();
+
+        if (rb == null)
+        {
+            rb = gameObject.AddComponent<Rigidbody>();
+        }
         if ( REST != null && Paper != null && PMD != null)
         {
             switch (trashType)
@@ -33,9 +41,17 @@ public class TrashGoingToWrongTrashBin : MonoBehaviour
             }
         }        
     }
-    void Update()
+    void FixedUpdate()
     {
-        //move to wrong trash bin in some way
+        if (target != null)
+        {
+            MoveTowardsTarget();
+        }
+    }
+    private void MoveTowardsTarget()
+    {
+        Vector3 direction = (target.transform.position - transform.position).normalized;
+        rb.AddForce(direction * 12f);
     }
     private GameObject ChooseRandomGameObjectOutOf2(GameObject obj1, GameObject obj2)
     {
