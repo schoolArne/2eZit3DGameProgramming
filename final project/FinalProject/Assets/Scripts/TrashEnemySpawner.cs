7847c4762c1e0d40c33399e80ec3e30e654ad513
Arne <arnev.c.23@gmail.com>
2024-08-07 19:01:00 +0200

    init trash enemy spawner script

---
+++ final project/FinalProject/Assets/Scripts/TrashEnemySpawner.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashEnemySpawner : MonoBehaviour
+{
+    public GameObject Trash1;
+    public GameObject Trash2;
+    public GameObject Trash3;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
