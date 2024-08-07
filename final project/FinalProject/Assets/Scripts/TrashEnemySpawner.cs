e8e06bae40605d04cfbcc1d166079a68062f648a
Arne <arnev.c.23@gmail.com>
2024-08-07 19:18:55 +0200

    make spawn interval inputs private

--- final project/FinalProject/Assets/Scripts/TrashEnemySpawner.cs
+++ final project/FinalProject/Assets/Scripts/TrashEnemySpawner.cs
@@ -7,8 +7,8 @@ public class TrashEnemySpawner : MonoBehaviour
     public GameObject Trash1;
     public GameObject Trash2;
     public GameObject Trash3;
-    public float minimumSpawnTime = 3f;
-    public float maximumSpawnTime = 10f;
+    private float minimumSpawnTime = 3f;
+    private float maximumSpawnTime = 10f;
     void Start()
     {
         StartCoroutine(SpawnEnemyAtRandomIntervals());
