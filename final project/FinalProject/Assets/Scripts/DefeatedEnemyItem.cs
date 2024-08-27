837245f358fc035002a525b4d88bd5e017907650
Arne <arnev.c.23@gmail.com>
2024-08-28 01:34:00 +0200

    update inventory upon colleting defeated enemies

--- final project/FinalProject/Assets/Scripts/DefeatedEnemyItem.cs
+++ final project/FinalProject/Assets/Scripts/DefeatedEnemyItem.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class DefeatedEnemyItem : MonoBehaviour
 {
+    public string correctInventoryReference = "";
     private float rotationSpeed = 100f;
     private float bobbingAmplitude = 0.5f;
     private float bobbingSpeed = 2f;
     private float initialY;
+    private GameManager gameManager;
     void Start()
     {
         initialY = transform.position.y;
+        gameManager = FindObjectOfType<GameManager>();
+        if (gameManager == null)
+        {
+            Debug.LogError("GameManager not found in the scene.");
+        }
     }
     void Update()
     {
@@ -25,5 +32,16 @@ public class DefeatedEnemyItem : MonoBehaviour
     {
         float newY = initialY + Mathf.Sin(Time.time * bobbingSpeed) * bobbingAmplitude;
         transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (gameManager != null || correctInventoryReference != "")
+            {
+                gameManager.UpdateTrashInventory(correctInventoryReference, 1);
+            }
+            Destroy(gameObject);
+        }
     }
 }
