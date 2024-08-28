230d8aef69a62d7e840da8e94fd0641b900e58b4
Arne <arnev.c.23@gmail.com>
2024-08-28 22:12:24 +0200

    show score on end screen

--- final project/FinalProject/Assets/Scripts/EndScene/GameManagerEndScene.cs
+++ final project/FinalProject/Assets/Scripts/EndScene/GameManagerEndScene.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManagerEndScene : MonoBehaviour
 {
+    private TMP_Text scoreText;
     void Start()
     {
         int trashInWrongTrashBin = PlayerPrefs.GetInt("trashinwrongtrashbin", 0);
         int restpickedup = PlayerPrefs.GetInt("restpickedup", 0);
         int pmdpickedup = PlayerPrefs.GetInt("pmdpickedup", 0);
         int paperpickedup = PlayerPrefs.GetInt("paperpickedup", 0);
-        Debug.Log(trashInWrongTrashBin + " " + restpickedup + " " + pmdpickedup + " " + paperpickedup);
+
+        GameObject scoreTextObject = GameObject.Find("Score");
+        if (scoreTextObject != null)
+        {
+            scoreText = scoreTextObject.GetComponent<TMP_Text>();
+            scoreText.text = "Trash that went into wrong trashbin: " + trashInWrongTrashBin + "\n\n" + "rest picked up: " + restpickedup + "\n" + "pmd picked up: " + pmdpickedup + "\n" + "paper picked up: " + paperpickedup;
+        }
     }
 }
