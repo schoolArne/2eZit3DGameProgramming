f51527db0556019e277ccd98cd8b32f2b950e2e0
Arne <arnev.c.23@gmail.com>
2024-08-28 21:58:21 +0200

    set game time back to 100 seconds

--- final project/FinalProject/Assets/Scripts/GameManager.cs
+++ final project/FinalProject/Assets/Scripts/GameManager.cs
@@ -16,7 +16,7 @@ public class GameManager : MonoBehaviour
     private TMP_Text remainingTimeText;
     private Dictionary<string, int> trashInventory = new Dictionary<string, int>();
     private int trashInWrongTrashBin = 0;
-    private int remainingTime = 50;
+    private int remainingTime = 100;
     private float timeSinceLastTick = 0f;
     private float tickRate = 1f;
     void Start()
