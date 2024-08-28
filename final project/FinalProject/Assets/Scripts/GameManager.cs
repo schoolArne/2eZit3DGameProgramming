01d4d89ec954831347d341c7290656f37fa6dcfd
Arne <arnev.c.23@gmail.com>
2024-08-28 19:20:46 +0200

    keep count of trash that entered wrong trashbin and represent count on screen

--- final project/FinalProject/Assets/Scripts/GameManager.cs
+++ final project/FinalProject/Assets/Scripts/GameManager.cs
@@ -11,12 +11,15 @@ public class GameManager : MonoBehaviour
     private int currentAmmo = 0;
     private TMP_Text ammoText;
     private TMP_Text inventoryText;
-    private Dictionary<string, int> trashInventory = new Dictionary<string, int>();    
+    private TMP_Text wrongTrashText;
+    private Dictionary<string, int> trashInventory = new Dictionary<string, int>();
+    private int trashInWrongTrashBin = 0;
     void Start()
     {
         currentAmmo = startAmmo;
         GameObject ammoTextObject = GameObject.Find("AmmoCount");
         GameObject inventoryTextObject = GameObject.Find("TrashInventoryInfo");
+        GameObject trashInWrongTrashBinObject = GameObject.Find("WrongTrashInfo");
         if (ammoTextObject != null)
         {
             ammoText = ammoTextObject.GetComponent<TMP_Text>();
@@ -24,6 +27,10 @@ public class GameManager : MonoBehaviour
         if (ammoTextObject != null)
         {
             inventoryText = inventoryTextObject.GetComponent<TMP_Text>();
+        }
+        if(trashInWrongTrashBinObject != null)
+        {
+            wrongTrashText = trashInWrongTrashBinObject.GetComponent<TMP_Text>();
         }
         trashInventory["PAPER"] = 0;
         trashInventory["REST"] = 0;
@@ -33,6 +40,7 @@ public class GameManager : MonoBehaviour
     {
         UpdateAmmoText(currentAmmo);
         UpdateInventoryText();
+        UpdateWrongTrashText();
     }
     public void ShootAndDecreaseAmmoCount()
     {
@@ -80,6 +88,13 @@ public class GameManager : MonoBehaviour
     }
     public void TrashWentIntoWrongTrashBin()
     {
-        Debug.Log("Trash went into wrong trashbin");
+        trashInWrongTrashBin++;
+    }
+    private void UpdateWrongTrashText()
+    {
+        if(wrongTrashText != null)
+        {
+            wrongTrashText.text = "Trash In Wrong Trashbin: " + trashInWrongTrashBin;
+        }
     }
 }
